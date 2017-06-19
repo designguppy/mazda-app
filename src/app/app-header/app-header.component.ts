5e06cd6064c056b0917759328433d95a54fd4022
Jordan Perrego <j_perrego@yahoo.com>
2017-06-19 14:36:16 -0400

    Created Header Component

---
+++ src/app/app-header/app-header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-header',
+  templateUrl: './app-header.component.html',
+  styleUrls: ['./app-header.component.css']
+})
+export class AppHeaderComponent implements OnInit {
+  private title: string;
+  constructor() { }
+
+  ngOnInit() {
+    this.title = "MCE DIGTIAL REMARKETING - TIRE COMMUNICATION";
+  }
+
+}
